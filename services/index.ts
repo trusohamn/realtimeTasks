391c3d8ce3dc457d01cfc5ca2cabbc8733522141
Marta Trusohamn <marta.trusohamn@gmail.com>
2023-08-09 11:12:27 +0200

    basic express

---
+++ services/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
