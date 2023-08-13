24aa1a033135c371b80f6e786aa9ca82ae4428b5
Marta Trusohamn <marta.trusohamn@gmail.com>
2023-08-13 20:08:26 +0200

    switch endpoints for dev

--- nextjs/constants.ts
+++ nextjs/constants.ts
@@ -1,2 +1,4 @@
-export const apiService = 'https://trusohamn-real-time-07344ac8fd63.herokuapp.com/api'
-export const wsService = 'wss://trusohamn-real-time-07344ac8fd63.herokuapp.com/socket'
+const isDev = process.env.NODE_ENV === "development";
+
+export const apiService = isDev ? 'http://localhost:8000/api' : 'https://trusohamn-real-time-07344ac8fd63.herokuapp.com/api'
+export const wsService = isDev ? 'ws://localhost:8000/socket' : 'wss://trusohamn-real-time-07344ac8fd63.herokuapp.com/socket'
